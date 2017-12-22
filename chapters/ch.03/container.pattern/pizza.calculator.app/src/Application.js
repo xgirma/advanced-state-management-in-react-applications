ed5acf16c3ef0c5488f1a850500b485ff32b665b
Girma Nigusse <xgirma@gmail.com>
2017-12-22 03:41:08 -0800

    ch.03

--- chapters/ch.03/container.pattern/pizza.calculator.app/src/Application.js
+++ chapters/ch.03/container.pattern/pizza.calculator.app/src/Application.js
@@ -4,36 +4,9 @@ import Title from './Title';
 import Input from './Input';
 import Result from './Result';
 
-import calculatePizzasNeeded from './lib/calculate-pizzas-needed';
-
-const initialState = {
-  numberOfPeople: 10,
-  slicesPerPerson: 2,
-};
-
 export default class Application extends Component {
-  state = { ...initialState };
-
-  updateNumberOfPeople = event => {
-    const numberOfPeople = parseInt(event.target.value, 10);
-    this.setState({ numberOfPeople });
-  };
-
-  updateSlicesPerPerson = event => {
-    const slicesPerPerson = parseInt(event.target.value, 10);
-    this.setState({ slicesPerPerson });
-  };
-
-  reset = event => {
-    this.setState({ ...initialState });
-  };
-
   render() {
-    const { numberOfPeople, slicesPerPerson } = this.state;
-    const numberOfPizzas = calculatePizzasNeeded(
-      numberOfPeople,
-      slicesPerPerson,
-    );
+    const { numberOfPeople, slicesPerPerson, numberOfPizzas, updateNumberOfPeople, updateSlicesPerPerson,  reset } = this.props;
 
     return (
       <div className="Application">
@@ -43,17 +16,17 @@ export default class Application extends Component {
           type="number"
           min={0}
           value={numberOfPeople}
-          onChange={this.updateNumberOfPeople}
+          onChange={updateNumberOfPeople}
         />
         <Input
           label="Slices Per Person"
           type="number"
           min={0}
           value={slicesPerPerson}
-          onChange={this.updateSlicesPerPerson}
+          onChange={updateSlicesPerPerson}
         />
         <Result amount={numberOfPizzas} />
-        <button className="full-width" onClick={this.reset}>
+        <button className="full-width" onClick={reset}>
           Reset
         </button>
       </div>
--- chapters/ch.03/container.pattern/pizza.calculator.app/src/index.js
+++ chapters/ch.03/container.pattern/pizza.calculator.app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Application from './Application';
+import ApplicationContainer from './ApplicationContainer'
 
 import './style.css';
 
-render(<Application />, document.getElementById('root'));
+render(<ApplicationContainer />, document.getElementById('root'));
